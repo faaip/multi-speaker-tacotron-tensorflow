bd55873b8273fd4bcc781ee0adee6d930b1cdf51
Frederik Tollund Juutilainen <frederik.juutilainen@alexandra.dk>
2019-05-02 15:02:35 +0200

    synthesizing on local cpu works

--- bulk_synthesizer.py
+++ bulk_synthesizer.py
@@ -7,3 +7,26 @@ synthesizer = Synthesizer()
 synthesizer.load(load_path, 1, None)
 
 # %%
+parser = argparse.ArgumentParser()
+parser.add_argument('--load_path', default=load_path)
+parser.add_argument('--sample_path', default="samples")
+parser.add_argument('--text', default="This is a test. Let's speak English.")
+parser.add_argument('--num_speakers', default=1, type=int)
+parser.add_argument('--speaker_id', default=0, type=int)
+parser.add_argument('--checkpoint_step', default=None, type=int)
+parser.add_argument('--is_korean', default=False, type=str2bool)
+config = parser.parse_args()
+# %%
+text = "This is a test. Let's speak English."
+speaker_id = [0]
+sample_path = "samples"
+# %%
+audio = synthesizer.synthesize(
+    texts=text,
+    base_path=sample_path,
+    speaker_ids=speaker_id,
+    attention_trim=False,
+    isKorean=False)[0]
+
+
+# %%
